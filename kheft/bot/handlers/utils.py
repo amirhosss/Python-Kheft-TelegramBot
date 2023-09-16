df6bcbaa978f16bf50bfee227fbf45d4f306d65b
amirhosss <amirhosseinleo@gmail.com>
2023-09-16 12:02:07 +0330

    `normalize_to_en` added

--- kheft/bot/handlers/utils.py
+++ kheft/bot/handlers/utils.py
@@ -11,8 +11,27 @@ def normalize_from_en(num: int):
         "8": "۸",
         "9": "۹",
     }
-    num = "{:,}".format(num)
+    num = "{:,}".format(num).replace(",", "،")
 
     return "".join(
         [persian_map[digit] if digit in persian_map.keys() else digit for digit in num]
+    )
+
+
+def normalize_to_en(num: str):
+    en_map = {
+        "۰": "0",
+        "۱": "1",
+        "۲": "2",
+        "۳": "3",
+        "۴": "4",
+        "۵": "5",
+        "۶": "6",
+        "۷": "7",
+        "۸": "8",
+        "۹": "9",
+    }
+
+    return "".join(
+        [en_map[digit] if digit in en_map.keys() else digit for digit in num]
     )
