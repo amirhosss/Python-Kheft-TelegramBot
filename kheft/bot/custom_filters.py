eeb1037a1ae657dff798218c09127485e2219c50
amirhosss <amirhosseinleo@gmail.com>
2023-09-24 17:52:56 +0330

    Log the Exception

--- kheft/bot/custom_filters.py
+++ kheft/bot/custom_filters.py
@@ -5,14 +5,19 @@ from kheft.config import configs
 
 
 class IsMember(SimpleCustomFilter):
-    key = 'is_member'
+    key = "is_member"
 
     def __init__(self, bot: AsyncTeleBot) -> None:
         self._bot = bot
 
     async def check(self, message: Message):
-        chat_memebr = await self._bot.get_chat_member(chat_id=configs.telegrambot_public_channel,
-        user_id=message.chat.id)
-        member_status = chat_memebr.status
+        try:
+            chat_memebr = await self._bot.get_chat_member(
+                chat_id=configs.telegrambot_public_channel, user_id=message.chat.id
+            )
+            member_status = chat_memebr.status
 
-        return member_status not in ['left', 'banned']
+            return member_status not in ["left", "banned"]
+        except Exception as e:
+            print(f"an error occurred during `is_member` by: {e}")
+            return False
