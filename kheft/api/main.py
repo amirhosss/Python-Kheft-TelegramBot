b4fb7b0a41653b03d92cb58366da0ee890a3a1c0
amirhosss <amirhosseinleo@gmail.com>
2023-09-29 16:23:46 +0330

    Lifespan created

--- kheft/api/main.py
+++ kheft/api/main.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Annotated, Union
+from contextlib import asynccontextmanager
 
 import uvicorn
 import telebot.async_telebot as aiotelebot
@@ -20,7 +21,21 @@ logger = aiotelebot.logger
 aiotelebot.logger.setLevel(logging.INFO)
 
 
-app = FastAPI(docs=None, redoc_url=None)
+@asynccontextmanager
+async def lifespan() -> None:
+    """Register webhook for telegram updates."""
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    logger.debug(f"updating webhook url, new: {WEBHOOK_URL}")
+    if not await bot.set_webhook(
+        url=WEBHOOK_URL, secret_token=WEBHOOK_SECRET_TOKEN, drop_pending_updates=True
+    ):
+        raise RuntimeError("unable to set webhook")
+
+    yield
+
+
+app = FastAPI(docs=None, redoc_url=None, lifespan=lifespan)
 
 
 @app.post("/")
@@ -37,19 +52,6 @@ async def process_webhook(
         return {"message": "Forbidden"}
 
     await bot.process_new_updates([Update.de_json(update)])
-
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-@app.on_event("startup")
-async def startup() -> None:
-    """Register webhook for telegram updates."""
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    logger.debug(f"updating webhook url, new: {WEBHOOK_URL}")
-    if not await bot.set_webhook(
-        url=WEBHOOK_URL, secret_token=WEBHOOK_SECRET_TOKEN, drop_pending_updates=True
-    ):
-        raise RuntimeError("unable to set webhook")
 
 
 uvicorn.run(
